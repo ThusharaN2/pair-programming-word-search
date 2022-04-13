c19d8dcf7759990d7295b5aef7b536cc738055af
Thushara <thusharan@hotmail.com>
2022-04-13 15:16:07 -0400

    used tranposition to do wordsearch through 2-D array

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,31 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const wordSearch = (letters, word) => {
+  const horizontalJoin = letters.map(ls => ls.join(''));
 
-module.exports = wordSearch
+  console.log("horizontalJoin: ", horizontalJoin);
+  for (let l of horizontalJoin) {
+    if (l.includes(word)) return true;
+  }
+  const transpose = function(matrix) {
+    const outputArray = [];
+    
+    for (let i = 0; i < matrix[0].length; i++) {
+      outputArray.push([]);
+      for (let j = 0; j < matrix.length; j++) {
+        // console.log("i =", i, "j =", j);
+        outputArray[i].push(matrix[j][i]);
+      }
+    }
+    return outputArray;
+  };
+    
+  const verticalFlip = transpose(letters);
+
+  const verticalJoin = verticalFlip.map(ls => ls.join(''));
+    
+  for (let l of verticalJoin) {
+    if (l.includes(word)) return true;
+  }
+  return false;
+};
+
+module.exports = wordSearch;
